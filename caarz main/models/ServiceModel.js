874df820890d2819d9ecb45b77b429d3daca0bd4
Vishaka Mohan <vishaka.mohan2019@vitstudent.ac.in>
2021-11-25 11:04:41 +0530

    book service + other changes

--- caarz main/models/ServiceModel.js
+++ caarz main/models/ServiceModel.js
@@ -2,15 +2,15 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var serviceModelSchema = new Schema({
-    customerImage: { type: String, required: true },
+    customerImage: { type: String},
     customerName: { type: String, required: true },
     customerEmail: { type: String, required: true },
     customerPhone: { type: String, required: true },
-    customerModel: { type: String, required: true },
-    customerType: { type: String, required: true },
-    customerDone: { type: String, required: true },
-    customerBill: { type: String, required: true },
-    Date: { type: String, required: true }
+    customerModel: { type: String },
+    customerType: { type: String },
+    customerDone: { type: String },
+    customerBill: { type: String },
+    Date: { type: String }
 });
 
 module.exports = mongoose.model("service", serviceModelSchema, "service");
